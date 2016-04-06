c6ff700bcef8be744b8ac9f536a82ed4f1524fb8
Felix Frank <Felix.Frank@Alumni.TU-Berlin.de>
2016-04-07 01:19:30 +0200

    properly initialize puppetlabs_spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,28 @@
 #
 # general requirements for mgmtgraph specs
 
-require 'puppet'
+require 'puppetlabs_spec_helper/puppet_spec_helper'
 require 'puppet/face'
 require 'puppetx/catalog_translation'
-gem 'rspec', '>=3.0.0'
-require 'rspec/expectations'
 
 RSpec.configure do |config|
   config.mock_with :mocha
+
+  Puppet::Test::TestHelper.initialize
+
+  config.before :all do
+    Puppet::Test::TestHelper.before_all_tests()
+  end
+
+  config.after :all do
+    Puppet::Test::TestHelper.after_all_tests()
+  end
+
+  config.before :each do
+    Puppet::Test::TestHelper.before_each_test()
+  end
+
+  config.after :each do
+    Puppet::Test::TestHelper.after_each_test()
+  end
 end
