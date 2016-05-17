e01ca96fa24271a0e92257172bfbfdd2e4d9a66f
Felix Frank <Felix.Frank@Alumni.TU-Berlin.de>
2016-05-18 00:26:38 +0200

    test for purpleidea's fix for (#2)

--- spec/integration/catalog_translation_spec.rb
+++ spec/integration/catalog_translation_spec.rb
@@ -9,4 +9,22 @@ describe "PuppetX::CatalogTranslation" do
     expect(graph['edges'][0]['to']).to   include('name' => '/tmp/bar')
     expect(graph['edges'].length).to be == 1
   end
+
+  it "uses deterministic names for edges that do not change regardless of context" do
+    manifest = "file { '/tmp/foo': } -> file { '/tmp/bar': } -> resources { 'cron': }"
+    graph = PuppetX::CatalogTranslation.to_mgmt(resource_catalog(manifest))
+    edge = graph['edges'][0]
+
+    [ [ "file { [ '/a', '/b' ]: } ->", "" ],
+      [ "file { [ '/a', '/b' ]: } ->", "-> file { '/c': }" ],
+      [ "file { [ '/a', '/b' ]: } ->", "<- file { '/c': }" ],
+      [ "file { [ '/a', '/b' ]: } <-", "<- file { '/c': }" ],
+      [ "", "<- file { [ '/a', '/b', '/c' ]: }" ],
+      [ "", "-> file { [ '/a', '/b', '/c' ]: }" ]
+    ].each do |pre, post|
+      graph = PuppetX::CatalogTranslation.to_mgmt(resource_catalog(pre + manifest + post))
+      expect(graph['edges']).to_not be_empty
+      expect(graph['edges']).to include(edge)
+    end
+  end
 end
