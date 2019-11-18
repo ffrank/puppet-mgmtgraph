9e12eb0dbad47058da1621a0d9cc696f389d2594
Felix Frank <felix.frank@alumni.tu-berlin.de>
2019-11-18 02:43:37 +0100

    pdk conversion

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,48 +1,54 @@
-# spec_helper.rb
-#
-# general requirements for mgmtgraph specs
+require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-facts'
 
-require 'puppetlabs_spec_helper/puppet_spec_helper'
-require 'puppet/face'
-require 'puppetx/catalog_translation'
+require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
 
-RSpec.configure do |config|
-  config.mock_with :mocha
+include RspecPuppetFacts
 
-  Puppet::Test::TestHelper.initialize
+default_facts = {
+  puppetversion: Puppet.version,
+  facterversion: Facter.version,
+}
 
-  config.before :all do
-    Puppet::Test::TestHelper.before_all_tests()
-  end
+default_fact_files = [
+  File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml')),
+  File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml')),
+]
 
-  config.after :all do
-    Puppet::Test::TestHelper.after_all_tests()
-  end
+default_fact_files.each do |f|
+  next unless File.exist?(f) && File.readable?(f) && File.size?(f)
 
-  config.before :each do
-    Puppet::Test::TestHelper.before_each_test()
-  end
-
-  config.after :each do
-    Puppet::Test::TestHelper.after_each_test()
+  begin
+    default_facts.merge!(YAML.safe_load(File.read(f), [], [], true))
+  rescue => e
+    RSpec.configuration.reporter.message "WARNING: Unable to load #{f}: #{e}"
   end
 end
 
-def resource_catalog(manifest)
-  compile_catalog(manifest).to_ral
+# read default_facts and merge them over what is provided by facterdb
+default_facts.each do |fact, value|
+  add_custom_fact fact, value
 end
 
-def compile_catalog(manifest)
-  Puppet[:code] = manifest
-  node = Puppet::Node.new('spec.example.net')
-  Puppet::Parser::Compiler.compile(node)
+RSpec.configure do |c|
+  c.default_facts = default_facts
+  c.before :each do
+    # set to strictest setting for testing
+    # by default Puppet runs at warning level
+    Puppet.settings[:strict] = :warning
+  end
+  c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
+  c.after(:suite) do
+  end
 end
 
-def ensure_core_module(core_module)
-  return if Gem::Version.new(Puppet.version) < Gem::Version.new('6.0.0')
-  face = Puppet::Face['module','1.0.0']
-  # silly hack to make sure we have a temporary vardir, although the
-  # location is in the environment path, which is bonkers
-  Puppet[:vardir] = Puppet[:environmentpath] + "/vardir"
-  face.install("puppetlabs-#{core_module}_core", { :target_dir => Puppet[:environmentpath] + "/production/modules" })
+# Ensures that a module is defined
+# @param module_name Name of the module
+def ensure_module_defined(module_name)
+  module_name.split('::').reduce(Object) do |last_module, next_module|
+    last_module.const_set(next_module, Module.new) unless last_module.const_defined?(next_module, false)
+    last_module.const_get(next_module, false)
+  end
 end
+
+# 'spec_overrides' from sync.yml will appear below this line
