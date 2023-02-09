9988f0722a166061e2ae685be833d2fb2e85528c
Felix Frank <felix.frank.de@gmail.com>
2023-02-09 21:45:53 +0100

    clean up catalog_translation spec

--- spec/unit/catalog_translation_spec.rb
+++ spec/unit/catalog_translation_spec.rb
@@ -43,48 +43,48 @@ describe "PuppetX::CatalogTranslation" do
     end
 
     it "always generates a header" do
-      result = PuppetX::CatalogTranslation.to_mgmt(empty_catalog)
+      result = subject.to_mgmt(empty_catalog)
       expect(result).to include 'graph'
       expect(result).to include 'comment'
     end
 
     it "loads specific translators for resource types" do
       PuppetX::CatalogTranslation::Type.expects(:translation_for).with(:file)
-      PuppetX::CatalogTranslation.to_mgmt(file_catalog)
+      subject.to_mgmt(file_catalog)
     end
 
     it "does not include dropped resources in the result hash" do
       PuppetX::CatalogTranslation::Type.new(:filebucket) { emit nil }
-      result = PuppetX::CatalogTranslation.to_mgmt(bucket_catalog)
+      result = subject.to_mgmt(bucket_catalog)
       expect(result['resources']).to_not include 'notify'
       expect(result['resources']).to_not include 'exec'
     end
 
     it "keeps dependency edges between supported resources" do
-      result = PuppetX::CatalogTranslation.to_mgmt(edge_catalog)
+      result = subject.to_mgmt(edge_catalog)
       expect(result['edges'][0]).to include(
         'from' => { 'kind' => 'file', 'name' => '/tmp/spec1' },
         'to'   => { 'kind' => 'file', 'name' => '/tmp/spec2' },
       )
     end
 
-#    # TODO: bring this back if and when we have ignored resources again
-#    it "discards edges that connect to an ignored resource" do
-#      # load file translator before stubbing
-#      #PuppetX::CatalogTranslation::Type.translation_for(:file)
-#      # make sure that no notify translator is loaded
-#      #PuppetX::CatalogTranslation::Type.expects(:load_translator).with(:notify).at_least_once
-#      result = PuppetX::CatalogTranslation.to_mgmt(edge_catalog)
-#      #raise result['edges'].inspect
-#      expect(result['edges'].length).to eq(1)
-#      expect(result['edges'][0]).to_not include(
-#        'to' => { 'kind' => 'notify', 'name' => 'this will not translate' },
-#      )
-#    end
+    it "discards edges that connect to an ignored resource" do
+      pending("re-introduction of at least on ignored resource")
+      # load file translator before stubbing
+      #PuppetX::CatalogTranslation::Type.translation_for(:file)
+      # make sure that no notify translator is loaded
+      #PuppetX::CatalogTranslation::Type.expects(:load_translator).with(:notify).at_least_once
+      result = subject.to_mgmt(edge_catalog)
+      #raise result['edges'].inspect
+      expect(result['edges'].length).to eq(1)
+      expect(result['edges'][0]).to_not include(
+        'to' => { 'kind' => 'notify', 'name' => 'this will not translate' },
+      )
+    end
 
     it "converts ruby symbols in the result to strings" do
-      PuppetX::CatalogTranslation.expects(:desymbolize)
-      PuppetX::CatalogTranslation.to_mgmt(empty_catalog)
+      subject.expects(:desymbolize)
+      subject.to_mgmt(empty_catalog)
     end
   end
 
@@ -92,7 +92,7 @@ describe "PuppetX::CatalogTranslation" do
     it "makes sure to purge the error log" do
       PuppetX::CatalogTranslation::Type.expects(:reset_error_log!)
       PuppetX::CatalogTranslation::Type.expects(:dump_error_log)
-      PuppetX::CatalogTranslation.stats(empty_catalog)
+      subject.stats(empty_catalog)
     end
   end
 
@@ -106,7 +106,7 @@ describe "PuppetX::CatalogTranslation" do
       'a complex structure' => { :k => [ :a, :b ] } }.each do |description,value|
       context "when given #{description}" do
         it "returns strings only" do
-          result = PuppetX::CatalogTranslation.desymbolize(value)
+          result = subject.desymbolize(value)
           if result.respond_to? :to_a
             result = result.to_a
           end
@@ -120,16 +120,16 @@ describe "PuppetX::CatalogTranslation" do
 
   describe "::set_mode" do
     it "accepts value :optimistic" do
-      expect { PuppetX::CatalogTranslation.set_mode(:optimistic) }.to_not raise_error
+      expect { subject.set_mode(:optimistic) }.to_not raise_error
     end
 
     it "accepts value :conservative" do
-      expect { PuppetX::CatalogTranslation.set_mode(:conservative) }.to_not raise_error
+      expect { subject.set_mode(:conservative) }.to_not raise_error
     end
 
     it "does not accept unknown parameter values" do
       [ :great, :weird, :amazing, :elastic ].each do |param|
-        expect { PuppetX::CatalogTranslation.set_mode(param) }.to raise_error(/invalid .* mode/)
+        expect { subject.set_mode(param) }.to raise_error(/invalid .* mode/)
       end
     end
   end
